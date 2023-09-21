facc7bc476f7383e05672c7cb70f6ea2191d596b
Test <dmuriithi.k@gmail.com>
2023-09-21 19:53:34 +0300

    learns precision for decimals

--- hello.rs
+++ hello.rs
@@ -1,6 +1,20 @@
 fn main(){
 	println!("Hello World");
 	println!("I am a rustacean");
+
+
+	println!("{} days is less than {}",31, 40);
+
+	println!("{subject} {verb} {object}", object="the lazy dog", subject="the quick brown fox", verb="jumps over");
+
+
+	println!("{number:0>10}", number=1);
+
+	println!("My name is {0}, {1} {0}", "Bond", "James");
+
+
+	println!("Pi is roughly {:.*}",3, 3.14152);
+
 }
 
 // This is my first rust code and this is a comment and cannot be executed. This is a regular comment
@@ -8,3 +22,5 @@ fn main(){
 /*
 This on the other hand is a block comment
 */
+
+
