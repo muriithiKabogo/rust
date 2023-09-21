aa81b28bdbcb7d55a6cbb1bcdac9ea0aec06e852
Test <dmuriithi.k@gmail.com>
2023-09-21 19:32:30 +0300

    initial commit

---
+++ hello.rs
@@ -0,0 +1,10 @@
+fn main(){
+	println!("Hello World");
+	println!("I am a rustacean");
+}
+
+// This is my first rust code and this is a comment and cannot be executed. This is a regular comment
+
+/*
+This on the other hand is a block comment
+*/
